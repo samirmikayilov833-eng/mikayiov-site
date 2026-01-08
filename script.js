7ea4399e98d48247747f1cfa4e8e44c128dce7bf
samirmikayilov833-eng <samirmikayilov833@gmail.com>
2026-01-08 15:08:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-console.log("Sayt üçün script.js işə düşdü");
+console.log("script.js işə düşdü");
 
+// Kameranı açma funksiyası (yalnız icazə verilsə)
+const video = document.querySelector("#cameraStream");
+const captureBtn = document.querySelector("#captureBtn");
+const canvas = document.querySelector("#snapshot");
+const result = document.querySelector("#result");
+
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch(err => {
+    console.log("Kamera açıla bilmədi:", err);
+  });
+
+// Şəkil çəkmə düyməsi
+captureBtn.addEventListener("click", () => {
+  const context = canvas.getContext("2d");
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  result.classList.add("animate");
+  setTimeout(() => result.classList.remove("animate"), 1000);
+});
