bd85c7f519de3416b6617589ccff06e6cc41550a
samirmikayilov833-eng <samirmikayilov833@gmail.com>
2026-01-10 10:53:56 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+const text = document.getElementById("text");
+
+// Yazı yavaş-yavaş böyüsün
+text.style.transition = "transform 0.8s";
+
+setInterval(() => {
+  text.style.transform = "scale(1.1)";
+  setTimeout(() => {
+    text.style.transform = "scale(1)";
+  }, 400);
+}, 800);
+
+// Klikləyəndə emoji əlavə olsun
+text.addEventListener("click", () => {
+  text.innerText += " 😊";
+});
 
 
