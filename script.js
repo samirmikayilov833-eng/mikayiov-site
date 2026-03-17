5b861bfb29e7bfa6be37b0a27314dfdf82f1c55f
samirmikayilov833-eng <samirmikayilov833@gmail.com>
2026-03-17 16:17:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = (Math.random() - 0.5) * 2;
+        this.speedY = (Math.random() - 0.5) * 2;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    draw() {
+        ctx.fillStyle = "rgba(255,255,255,0.7)";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function createParticles(x, y) {
+    for (let i = 0; i < 5; i++) {
+        particles.push(new Particle(x, y));
+    }
+}
+
+window.addEventListener("mousemove", (e) => {
+    createParticles(e.x, e.y);
+});
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach((p, index) => {
+        p.update();
+        p.draw();
+
+        if (p.size <= 0.2) {
+            particles.splice(index, 1);
+        }
+
+        p.size -= 0.02;
+    });
+
+    requestAnimationFrame(animate);
+}
+
+animate();
